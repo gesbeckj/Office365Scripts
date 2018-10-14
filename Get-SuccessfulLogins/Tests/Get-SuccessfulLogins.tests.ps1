f78a2ee5766445a30357b026055fa33866e0f17b
gesbeckj <james@gesbeck.com>
2018-10-14 18:42:58 -0500

    Test fixes

--- Get-SuccessfulLogins/Tests/Get-SuccessfulLogins.tests.ps1
+++ Get-SuccessfulLogins/Tests/Get-SuccessfulLogins.tests.ps1
@@ -8,12 +8,12 @@ Describe "Testing Get-SuccessfulLogins"{
         (Test-Path "$here\..\Get-SuccessfulLogins.ps1") | Should Be $True
     }
     It "Should not throw"{
-        {Get-IMAPLogins} | Should Not Throw
+        {Get-SuccessfulLogins} | Should Not Throw
     }
     It "If no logins exists should return null"{
-        Get-IMAPLogins | Should Be $null
+        Get-SuccessfulLogins | Should Be $null
     }
     It "If Logins Exist should not return null"{
-        Get-IMAPLogins | Should Not Be $null
+        Get-SuccessfulLogins | Should Not Be $null
     }
 }
