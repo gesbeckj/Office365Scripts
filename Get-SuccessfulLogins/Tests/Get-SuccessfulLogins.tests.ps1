468014bd02af57429fe6263d6fc8f45b28265558
gesbeckj <james@gesbeck.com>
2018-10-14 20:03:10 -0500

    Update Get-SuccessfulLogins/Tests/Get-SuccessfulLogins.tests.ps1

--- Get-SuccessfulLogins/Tests/Get-SuccessfulLogins.tests.ps1
+++ Get-SuccessfulLogins/Tests/Get-SuccessfulLogins.tests.ps1
@@ -8,14 +8,14 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 Describe "Testing Get-SuccessfulLogins"{
     It "Script Should Exists"{
-        (Test-Path "$here\Get-SuccessfulLogins.ps1") | Should Be $True
+        (Test-Path "$here\..\Get-SuccessfulLogins.ps1") | Should Be $True
     }
     It "Should not throw"{
-        Mock -CommandName Get-Office65AuditLogs -MockWith {Return $null}
+        Mock -CommandName Get-Office365AuditLogs -MockWith {Return $null}
         {Get-SuccessfulLogins} | Should Not Throw
     }
     It "If no logins exists should return null"{
-        Mock -CommandName Get-Office65AuditLogs -MockWith {Return $null}
+        Mock -CommandName Get-Office365AuditLogs -MockWith {Return $null}
         Get-SuccessfulLogins | Should Be $null
     }
     #It "If Logins Exist should not return null"{
