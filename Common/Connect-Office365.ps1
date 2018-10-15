82396ac44458ff567f336c5d6044e5b12c383e67
gesbeckj <james@gesbeck.com>
2018-10-14 20:49:26 -0500

    Added Mutli Tenant Steps

--- Common/Connect-Office365.ps1
+++ Common/Connect-Office365.ps1
@@ -2,7 +2,9 @@ function Connect-Office365
 {
     [CmdletBinding()]
     param(
-    [switch]$ConnectMSOLOnly
+    [switch]$ConnectMSOLOnly,
+    [switch]$SkipMSOL,
+    [string]$tenantDomainName = ""
     )
     #Attempt to Import the MSOnline Module
     try {
@@ -13,15 +15,25 @@ function Connect-Office365
         return $null
     }
     #Get User Credentials
+    if (-not ($tenantDomainName -eq ""))
+    {
     $Credential = Get-Credential
+    }
     if ($null -eq $Credential)
     {
         Write-Error "No credentials entered"
     }
-    Connect-MsolService -Credential $Credential
-    if (-not ($ConnectMSOLOnly))
+    if (-not ($SkipMSOL))
+    {
+        Connect-MsolService -Credential $Credential
+    }
+    if ((-not ($ConnectMSOLOnly)) -and $tenantDomainName -eq "")
     {
         $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $Credential -Authentication Basic -AllowRedirection
+    }
+    elseif (-not ($ConnectMSOLOnly)){
+        $DelegatedAdminCred = Get-Credential -Message "Enter delegated administrative credentials. This will not work with MFA"
+        $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://ps.outlook.com/powershell-liveid?DelegatedOrg=$tenantDomainName" -Credential $DelegatedAdminCred  -Authentication Basic -AllowRedirection
     }
     return $Session
 }
