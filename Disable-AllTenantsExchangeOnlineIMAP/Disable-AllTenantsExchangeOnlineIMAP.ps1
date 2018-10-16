6cbd5c657e17389e4e11cf772260b00278452c8a
gesbeckj <james@gesbeck.com>
2018-10-15 20:47:42 -0500

    Added IMAP Disbling for MultiTenant

---
+++ Disable-AllTenantsExchangeOnlineIMAP/Disable-AllTenantsExchangeOnlineIMAP.ps1
@@ -0,0 +1,25 @@
+Function Disable-AllTenantsExchangeOnlineIMAP {
+    [CmdletBinding()]
+    param (
+        [parameter(
+            Mandatory = $false,
+            ValueFromPipelineByPropertyName = $true)]
+        [psobject[]]$TenantsList
+    )
+
+    $here = Split-Path -Parent $MyInvocation.MyCommand.Path
+    . "$here\..\Common\Connect-Office365.ps1"
+    . "$here\..\Disable-TenantExchangeOnlineIMAP\Disable-TenantExchangeOnlineIMAP.ps1"
+    if ($Null -eq $TenantsList) {
+        $session = Connect-Office365 -ConnectMSOLOnly
+        $session | out-null
+        $tenants = Get-MsolPartnerContract
+    }
+    Else {
+        $tenants = $TenantsList
+    }
+    $DelegatedAdminCred = Get-Credential -Message "Enter delegated administrative credentials. This will not work with MFA"
+    foreach ($tenant in $tenants) {
+        Disable-TenantExchangeOnlineIMAP -TenantDomainName $tenant -DelegatedAdminCred $DelegatedAdminCred
+    }
+}
---
+++ Disable-TenantExchangeOnlineIMAP/Disable-TenantExchangeOnlineIMAP.ps1
@@ -0,0 +1,38 @@
+Function Disable-TenantExchangeOnlineIMAP {
+    [CmdletBinding()]
+    param (
+        [string]$TenantDomainName,
+        [pscredential]$DelegatedAdminCred
+    )
+    if ($PSScriptRoot -eq $null) {
+        $here = Split-Path -Parent $MyInvocation.MyCommand.Path
+    }
+    else {
+        $here = $PSScriptRoot
+    }
+
+    . "$here\..\Common\Connect-TenantExchangeOnline.ps1"
+    $session = Connect-TenantExchangeOnline -TenantDomainName $TenantDomainName -DelegatedAdminCred $DelegatedAdminCred
+    if ($null -eq $session) {
+        Write-Error "Connection to Office 365 Failed"
+        throw "Unable to Connect to Office 365"
+    }
+    Import-PSSession -Session $session
+    #Disable IMAP & POP in CAS Mailbox Plans
+    Get-CASMailboxPlan -Filter 'ImapEnabled -eq "true" -or PopEnabled -eq "true" ' | set-CASMailboxPlan -ImapEnabled $false -PopEnabled $false
+
+    #Verify this was successful.
+    $Results = Get-CASMailboxPlan -Filter '{ImapEnabled -eq "true" -or PopEnabled -eq "true"'
+    if ($null -ne $Results) {
+        Throw "Unable to Disable IMAP and POP in CAS Mailbox Plans"
+    }
+
+    #Disable IMAP & POP on existing Mailboxes
+    Get-CASMailbox -Filter 'ImapEnabled -eq "true" -or PopEnabled -eq "true" ' | Select-Object @{n = "Identity"; e = {$_.primarysmtpaddress}} | Set-CASMailbox -ImapEnabled $false -PopEnabled $false
+    $Results = Get-CASMailbox -Filter 'ImapEnabled -eq "true" -or PopEnabled -eq "true"'
+    if ($null -ne $Results) {
+        Throw "Unable to Disable IMAP and POP on all existing mailboxes."
+    }
+    Write-Information "IMAP / POP are fully disabled"
+    return 0
+}
