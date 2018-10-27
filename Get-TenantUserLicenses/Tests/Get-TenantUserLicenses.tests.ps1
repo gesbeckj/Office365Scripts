d47f71cd3db8963e0f4238eea4a7dec284400b4b
gesbeckj <james@gesbeck.com>
2018-10-27 18:38:42 -0500

    Update Get-TenantUserLicenses/Tests/Get-TenantUserLicenses.tests.ps1

--- Get-TenantUserLicenses/Tests/Get-TenantUserLicenses.tests.ps1
+++ Get-TenantUserLicenses/Tests/Get-TenantUserLicenses.tests.ps1
@@ -3,9 +3,20 @@ param()
 
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 . "$here\..\Get-TenantUserLicenses.ps1"
+. "$here\..\..\Common\Connect-TenantExchangeOnline.ps1"
+. "$here\..\..\Common\Get-LicenseName.ps1"
+. "$here\..\..\Common\Connect-Office365.ps1"
 Describe "Get-TenantUserLicenses" {
     It "Script Should Exists" {
         (Test-Path "$here\..\Get-TenantUserLicenses.ps1") | Should Be $True
     }
-    It ""
+    It 'If it cannot connect to Office 365 it should throw'{
+        $password = 'bogus' | ConvertTo-SecureString -AsPlainText -Force
+        $cred = New-Object pscredential('bogus', $password)
+        function Get-Credential {
+            return $cred 
+        }
+        Mock -Command Connect-TenantExchangeOnline {Return $null}
+        {Get-TenantUserLicenses -ErrorAction SilentlyContinue } | Should Throw
+    }
 }
