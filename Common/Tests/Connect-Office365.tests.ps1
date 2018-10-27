f4e274f0df074349e9858f72c90834f1ef4e2774
gesbeckj <james@gesbeck.com>
2018-10-27 15:53:13 -0500

    Update Common/Tests/Connect-Office365.tests.ps1

--- Common/Tests/Connect-Office365.tests.ps1
+++ Common/Tests/Connect-Office365.tests.ps1
@@ -4,24 +4,22 @@ param()
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 . "$here\..\Connect-Office365.ps1"
 Import-Module MSOnline
-Describe "Testing Connect-Office365"{
-    It "Script Should Exists"{
+Describe "Connect-Office365" {
+    It "Script Should Exists" {
         (Test-Path "$here\..\Connect-Office365.ps1") | Should Be $True
     }
-    Context 'MSOnline Missing'{
+    it 'Should return $Null if MSOnline Missing' {
         Mock -CommandName "Import-Module" {throw "Oops"}
-        it 'Should return $Null if MSOnline Missing'{
-            Connect-Office365 -ErrorAction SilentlyContinue | Should Be $Null
+        Connect-Office365 -ErrorAction SilentlyContinue | Should Be $Null
+    }
+    It 'Should not return $Null' {
+        $password = 'bogus' | ConvertTo-SecureString -AsPlainText -Force
+        $cred = New-Object pscredential('bogus', $password)
+        function Get-Credential {
+            return $cred 
         }
+        Mock -CommandName New-PSSession {return "Session"}
+        Mock -CommandName Connect-MsolService {return "Session"}
+        Connect-Office365 | Should Be $Null
     }
-    Context 'Should Return'{
-    $password = 'bogus' | ConvertTo-SecureString -AsPlainText -Force
-    $cred = New-Object pscredential('bogus', $password)
-    function Get-Credential { return $cred }
-    Mock -CommandName New-PSSession {return "Session"}
-    Mock -CommandName Connect-MsolService {return "Session"}
-    It "Should not be Null normally"{
-        Connect-Office365 | Should -Not -be $null
-    }
-}
 }
