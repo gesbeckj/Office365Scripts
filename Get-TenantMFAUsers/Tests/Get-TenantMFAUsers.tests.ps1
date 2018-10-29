df1e1183184d2c00fd888645850081997aadac17
gesbeckj <james@gesbeck.com>
2018-10-28 19:43:20 -0500

    Update Get-TenantMFAUsers/Tests/Get-TenantMFAUsers.tests.ps1

--- Get-TenantMFAUsers/Tests/Get-TenantMFAUsers.tests.ps1
+++ Get-TenantMFAUsers/Tests/Get-TenantMFAUsers.tests.ps1
@@ -9,10 +9,37 @@ $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 Describe "Get-TenantMFAUsers" {
     Function Get-MsolPartnerContract {}
     Function Get-MsolUser {}
+    $StrongAuth = New-Object PSObject -Property @{
+        State = "Enforced"
+    }
+    $UserOne = New-Object PSObject -Property @{
+        Licenses    = "O365"
+        StrongAuthenticationRequirements = $strongAuth
+    }
+    $WeakAuth = New-Object PSObject -Property @{
+        State = "Enforced"
+    }
+    $UserTwo = New-Object PSObject -Property @{
+        Licenses    = "O365"
+        StrongAuthenticationRequirements = $WeakAuth
+    }
     It "Script Should Exists" {
         (Test-Path "$here\..\Get-TenantMFAUsers.ps1") | Should Be $True
     }
     It "Should not throw"{
         {Get-TenantMFAUsers} | Should Not Throw
+    }
+    It "Should return one result if there is one MFA user"{
+        Mock -CommandName Get-MSOLUser {Return $userOne}
+        $Result = Get-TenantMFAUsers
+        $Result | Should Not Be Null
+    }
+    It "Should return two results if there are two MFA users"{
+        $Users = @()
+        $Users += $userOne
+        $users += $userTwo
+        Mock -CommandName Get-MSOLUser {Return $users}
+        $Result = Get-TenantMFAUsers
+        $Result.count | Should Be 2
     }
 }
