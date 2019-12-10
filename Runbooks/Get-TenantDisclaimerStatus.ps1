ed3b357fe1dfc18e86b44d016217304e7f3f570c
James Gesbeck <james@gesbeck.com>
2019-12-10 17:21:23 -0600

    Create Get-TenantDisclaimerStatus.ps1

---
+++ Runbooks/Get-TenantDisclaimerStatus.ps1
@@ -0,0 +1,75 @@
+    param (
+        [string]$TenantDomainName
+    )
+# Ensures that any credentials apply only to the execution of this runbook
+Disable-AzureRmContextAutosave –Scope Process | Out-Null
+
+
+$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName 
+
+    "Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+
+$KeyVault = Get-AzureRmKeyVault
+$databaseName = Get-AzureKeyVaultSecret -VaultName $keyVault.VaultName -Name 'databasename'
+$sqlServerFQDN = Get-AzureKeyVaultSecret -VaultName $keyVault.VaultName -Name 'sqlServerFQDN'
+$sqlAdministratorLogin = Get-AzureKeyVaultSecret -VaultName $keyVault.VaultName -Name 'sqlAdministratorLogin'
+$sqlAdministratorLoginPassword = Get-AzureKeyVaultSecret -VaultName $keyVault.VaultName -Name 'sqlAdministratorLoginPassword'
+$Office365UPN = Get-AzureKeyVaultSecret -VaultName $keyVault.VaultName -Name 'Office365Login'
+$Office365RefreshToken = Get-AzureKeyVaultSecret -VaultName $keyVault.VaultName -Name 'Office365LoginPassword'
+
+
+$params = @{
+    'Database' = $databaseName.SecretValueText
+    'ServerInstance' = $sqlServerFQDN.SecretValueText
+    'Username' = $sqlAdministratorLogin.SecretValueText
+    'Password' = $sqlAdministratorLoginPassword.SecretValueText
+    'OutputSqlErrors' = $true
+    'Query' = 'SELECT GETDate()'
+}
+
+
+
+$URI = 'https://raw.githubusercontent.com/gesbeckj/Office365Scripts/Dev/Common/Connect-Office365.ps1'
+$TempFile = $Env:Temp + '\Common\Connect-Office365.ps1'
+Invoke-WebRequest -Uri $URI -OutFile $TempFile
+. $TempFile
+
+$URI = 'https://raw.githubusercontent.com/gesbeckj/Office365Scripts/Dev/Common/Connect-TenantExchangeOnline.ps1'
+$TempFile = $Env:Temp + '\Common\Connect-TenantExchangeOnline.ps1'
+Invoke-WebRequest -Uri $URI -OutFile $TempFile
+. $TempFile
+
+$session = Connect-TenantExchangeOnline -TenantDomainName $TenantDomainName -UPN $Office365UPN -ExchangeRefreshToken $Office365RefreshToken
+if ($null -eq $session) {
+    Write-Error "Connection to Office 365 Failed"
+    throw "Unable to Connect to Office 365"
+}
+$ImportSession = Import-PSSession -Session $session | Out-Null
+$DisclaimerRules = Get-TransportRule | Where-Object {$_.State -eq "Enabled" -and $_.Mode -eq "Enforce" -and $_.FromScope -eq "NotInOrganization" -and `
+$_.actions -eq "Microsoft.Exchange.MessagingPolicies.Rules.Tasks.ApplyHtmlDisclaimerAction" -and $null -eq $_.ExceptIfSenderDomainIs}
+Remove-PSSession -Session $session
+if ($null -eq $DisclaimerRules) {
+    return $false
+} else {
+    return $true
+}
