92b06963df1db32f4fc4245abfac6f62f7e5c225
James Gesbeck <james@gesbeck.com>
2022-03-01 16:06:43 -0600

    Update Get-AllTenantHostedOutboundSpamFilterPolicy.ps1

--- Runbooks/Get-AllTenantHostedOutboundSpamFilterPolicy.ps1
+++ Runbooks/Get-AllTenantHostedOutboundSpamFilterPolicy.ps1
@@ -107,4 +107,4 @@ foreach ($tenant in $tenants) {
     $params = @{"TenantDomainName"=$tenant.DefaultDomainName;"TenantName"=$tenant.name}
     Start-Sleep -seconds 10
 
-$job = Start-AzureRmAutomationRunbook -Name Get-TenantHostedOutboundSpamFilterPolicy -AutomationAccountName "AzureAutomation" -ResourceGroupName "PowerBI" -Parameters $params}
+$job = Start-AzureRmAutomationRunbook -Name Get-TenantHostedOutboundSpamFilterPolicy -AutomationAccountName "AzureAutomation" -ResourceGroupName "AberdeanPowerBI" -Parameters $params}
